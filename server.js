1b020d744f9fa692f750f7d99066bfcb31b41dff
mwafy092 <mwafy092@gmail.com>
2020-07-28 17:34:05 +0200

    JS update 2

--- server.js
+++ server.js
@@ -17,14 +17,30 @@ app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 const cors = require('cors');
+const { response } = require('express');
 app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 // Spin up the server
-const port = 3000;
+const port = 3003;
 const server = app.listen(port, () => {
     console.log(`Server is running on localhost: ${port}`);
 })
 
+
+// post route
+const data = []
+app.post('/postData', (request, response) => {
+    projectData['date'] = request.body.date;
+    projectData['temp'] = request.body.temp;
+    projectData['content'] = request.body.content;
+    response.send(projectData);
+    console.log(projectData);
+})
+
+// get route
+app.get('/allData', (request, response) => {
+    response.send(projectData);
+})
--- website/app.js
+++ website/app.js
@@ -1,21 +1,64 @@
+
 // Personal API Key for OpenWeatherMap API
 const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 const api = '5667a9881799123df15222c58542c78b';
 
 // getting data from user input
-document.getElementById('generate-btn').addEventListener('click', () => {
+document.getElementById('generate').addEventListener('click', () => {
     const zip = document.getElementById('zip').value;
-    const feeling = document.getElementById('feeling').value;
-    getWeather(baseURL, zip, code, api);
+    const feeling = document.getElementById('feelings').value;
+    const date = new Date();
+    getWeather(baseURL, zip, api)
+        .then(data => {
+            postData('/postData', { date: date, temp: data.main.temp, content: feeling });
+        })
+        .then(data => {
+            updateUI();
+        })
 })
 
 // get weather fetch function
-let getWeather = async (url, zip, code, api) => {
+let getWeather = async (url, zip, api) => {
     const newURL = `${url}${zip}&appid=${api}`
     const response = await fetch(newURL);
     try {
         const data = await response.json();
-        console.log(data);
+        return data;
+    }
+    catch (error) {
+        console.log('Error:', error);
+    }
+}
+
+// get route to fetch data from server
+const updateUI = async () => {
+    const request = await fetch('/allData');
+    try {
+        const allData = await request.json()
+        console.log(allData)
+        // update new entry values
+        document.getElementById('date').innerHTML = 'Date: ' + allData.date;
+        document.getElementById('temp').innerHTML = 'Temperature: ' + allData.temp;
+        document.getElementById('content').innerHTML = 'User Entry: ' + allData.content;
+    }
+    catch (error) {
+        console.log("error", error);
+    }
+};
+
+// post route to post data to server
+const postData = async (url = '', data = {}) => {
+    const request = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+
+    try {
+        const data = request.json();
         return data;
     }
     catch (error) {
